abe22dc5038e9b21b7023af8657444fcadcaa7b3
Jacky Lao <laojackyy@icloud.com>
2021-06-15 15:21:11 +0800

    Add SearchResultsViewController files

---
+++ GitSearch/SearchResultsViewController/SearchResultsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResultsViewController.swift
+//  GitSearch
+//
+//  Created by Jacky Lao on 6/15/21.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController, StoryboardInstantiatable {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
