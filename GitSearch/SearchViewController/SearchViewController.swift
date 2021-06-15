ea1fbc60ed06709c820758bd556145a724322331
Jacky Lao <laojackyy@icloud.com>
2021-06-15 15:22:41 +0800

    Add searchbar in SearchViewController

--- GitSearch/SearchViewController/SearchViewController.swift
+++ GitSearch/SearchViewController/SearchViewController.swift
@@ -9,10 +9,42 @@ import UIKit
 
 final class SearchViewController: UIViewController, StoryboardInstantiatable {
 
+    let searchResultsViewController = SearchResultsViewController.instantiate()
+    var searchController = UISearchController()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         title = "GitSearch"
+        
+        searchController = UISearchController(searchResultsController: searchResultsViewController)
+        searchController.searchBar.placeholder = "Search"
+        searchController.searchBar.delegate = self
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
     }
 
+    private func search(term: String) {
+        searchController.searchBar.text = term
+        searchController.isActive = true
+        searchController.searchBar.resignFirstResponder()
+    }
 }
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text else { return }
+        search(term: text)
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text, !text.isEmpty else { return }
+    }
+}
+
