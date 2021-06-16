72fbce261ccc33855334eee8d21b247b78de003f
Jacky Lao <laojackyy@icloud.com>
2021-06-16 20:08:49 +0800

    Add tableview with custom cell in SearchResultsViewController

---
+++ GitSearch/SearchResultsViewController/ResultTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ResultTableViewCell.swift
+//  GitSearch
+//
+//  Created by Jacky Lao on 6/16/21.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+
+    static let identifier = "ResultTableViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
--- GitSearch/SearchResultsViewController/SearchResultsViewController.swift
+++ GitSearch/SearchResultsViewController/SearchResultsViewController.swift
@@ -9,8 +9,28 @@ import UIKit
 
 class SearchResultsViewController: UIViewController, StoryboardInstantiatable {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    
+    func updateResults(_ results: [String]) {
+        tableView.reloadData()
+    }
+}
+
+extension SearchResultsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ResultTableViewCell.identifier, for: indexPath) as! ResultTableViewCell
+
+        return cell
+    }
+    
 }
